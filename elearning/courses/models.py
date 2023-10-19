71f1f9a77d2b99468750af4be30a019d0bbe0c88
AndrewDya <andrewdya@gmail.com>
2023-10-19 11:25:07 +0600

    models creadted, make migrations

--- elearning/courses/models.py
+++ elearning/courses/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    video_link = models.URLField()
+    duration_seconds = models.IntegerField()
+    products = models.ManyToManyField(Product, related_name='lessons')
+
+
+class LessonView(models.Model):
+    STATUS_CHOICES = [
+        ("Просмотрено", "Просмотрено"),
+        ("Не просмотрено", "Не просмотрено"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    viewed_time_seconds = models.IntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
